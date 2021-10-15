72b403fd1acc84f893e16962bdad3eb8774c75d4
ThanhTrung09 <kominam9x@email.com>
2021-10-15 14:36:16 +0700

    add vector icon

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function HomeScreen() {
     return (
         <View>
             <Text style={styles.container}>Lương đã ứng trong tháng</Text>
-            <Text style={{ fontSize: 18, fontStyle: 'normal', fontFamily: 'SF-Pro-Display-Black' }}>Lương đã ứng trong tháng</Text>
+            <Icon name="rocket" size={40} color="#900" />
         </View>
     )
 }
